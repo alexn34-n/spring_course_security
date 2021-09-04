62eccda33e38c4fa823708ad3e57735b78221e0b
alexn34-n <alexn3469@mail.ru>
2021-09-04 21:40:32 +0300

    Initial commit

---
+++ src/main/java/com/alexn/spring/security/configuration/MySecurityConfig.java
@@ -0,0 +1,24 @@
+package com.alexn.spring.security.configuration;
+
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        UserBuilder userBuilder= User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("ivan").password("ivan").roles("EMPLOYEE"))
+                .withUser(userBuilder.username("oleg").password("oleg").roles("HP"))
+                .withUser(userBuilder.username("nina").password("nina").roles("MANAGER","HR"));
+    }
+}
---
+++ src/main/java/com/alexn/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.alexn.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
