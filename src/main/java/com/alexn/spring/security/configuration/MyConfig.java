6856ec968d00dce4a19876d5c33e67c949eedb3b
alexn34-n <alexn3469@mail.ru>
2021-09-22 18:22:30 +0300

    Пароль в Database

--- src/main/java/com/alexn/spring/security/configuration/MyConfig.java
+++ src/main/java/com/alexn/spring/security/configuration/MyConfig.java
@@ -1,12 +1,15 @@
 package com.alexn.spring.security.configuration;
 
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "com.alexn.spring.security")
@@ -23,4 +26,20 @@ public class MyConfig {
 
         return internalResourceViewResolver;
     }
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource= new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&serverTimezone=UTC");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuser");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+
 }
